bc8e483474bcaca4332ded716de05fd961258547
FVS-MBA <feliven@gmail.com>
2026-07-27 18:10:34 -0300

    implementa signal e computed

--- src/app/signals-intro/signals-intro.ts
+++ src/app/signals-intro/signals-intro.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+export interface Elemento {
+  nome: string;
+  simbolo: string;
+  numeroMassa: number;
+}
 
 @Component({
   selector: 'app-signals-intro',
@@ -6,4 +12,26 @@ import { Component } from '@angular/core';
   templateUrl: './signals-intro.html',
   styleUrl: './signals-intro.css',
 })
-export class SignalsIntro {}
+export class SignalsIntro {
+  protected elementoSelecionado = signal<Elemento | null>(null);
+
+  elementos: Elemento[] = [
+    { nome: 'Hidrogênio', simbolo: 'H', numeroMassa: 1 },
+    { nome: 'Carbono', simbolo: 'C', numeroMassa: 12 },
+    { nome: 'Nitrogênio', simbolo: 'N', numeroMassa: 14 },
+    { nome: 'Oxigênio', simbolo: 'O', numeroMassa: 16 },
+    { nome: 'Sódio', simbolo: 'Na', numeroMassa: 23 },
+    { nome: 'Cloro', simbolo: 'Cl', numeroMassa: 35 },
+  ];
+
+  selecionarElemento(elemento: Elemento) {
+    this.elementoSelecionado.set(elemento);
+  }
+
+  exibirInfoElemento = computed(() => {
+    const elemento = this.elementoSelecionado();
+    return elemento
+      ? `Nome: ${elemento.nome}, Símbolo: (${elemento.simbolo}), Número de Massa: ${elemento.numeroMassa}`
+      : 'Nenhum elemento selecionado';
+  });
+}
